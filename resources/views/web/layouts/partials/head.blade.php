256f77524863edccc4afdcdbe14cb8e5d7c02a65
dedy priyatna <57609978+dedypry@users.noreply.github.com>
2025-05-21 21:31:39 +0700

    update

--- resources/views/web/layouts/partials/head.blade.php
+++ resources/views/web/layouts/partials/head.blade.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="utf-8">
-    <title>iDESIGN - Interior Design HTML Template</title>
+    <title>90Home - Agent Penjualan Rumah</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free HTML Templates" name="keywords">
     <meta content="Free HTML Templates" name="description">
