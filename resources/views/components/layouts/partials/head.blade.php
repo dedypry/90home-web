eff3a3f75f3780e923f7e72c40b260d25d693a08
dedy priyatna <57609978+dedypry@users.noreply.github.com>
2025-05-23 15:37:03 +0700

    update

--- resources/views/components/layouts/partials/head.blade.php
+++ resources/views/components/layouts/partials/head.blade.php
@@ -2,8 +2,8 @@
     <meta charset="utf-8">
     <title>{{ $title ?? '90Home - Agent Penjualan Rumah' }}</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="Free HTML Templates" name="keywords">
-    <meta content="Free HTML Templates" name="description">
+    <meta content="Web Agent Penjualan Rumah" name="keywords">
+    <meta content="Web Agent Penjualan Rumah" name="description">
 
     <!-- Favicon -->
     <link href="{{asset('web/logo.PNG')}}" rel="icon">
